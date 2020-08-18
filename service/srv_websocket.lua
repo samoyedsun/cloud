b51c024f28a0751719060e88fcb660ae14118e67
John Ma <samoyedsun@gmail.com>
2020-08-18 21:08:00 +0800

    fix bug about websocket

--- service/srv_websocket.lua
+++ service/srv_websocket.lua
@@ -1,10 +1,8 @@
 local skynet = require "skynet"
 local socket = require "skynet.socket"
-local service = require "skynet.service"
 local websocket = require "http.websocket"
 
-local port, wsapp_name , mode = ...
-local handle = require(wsapp_name)
+local port, wsapp_name, protocol = ...
 
 local CMD = {}
 local agent = {}
@@ -14,48 +12,49 @@ local listen_id
 local agent_num = thread * 2
 
 function CMD.update()                           -- 热更新
-
+    local old_agent = agent
+    local new_agent = {}
+    update_count = update_count + 1             -- 更新次数
+    for i = 1, agent_num do 
+        new_agent[i] = skynet.newservice("srv_web_agent", webapp_name, body_size_limit, "update:"..update_count)
+    end
+    
+    agent = new_agent
+    for _, v in ipairs(old_agent) do
+        skynet.send(v, "lua", "update")
+    end
 end
 
 function CMD.exit()
-
+    socket.close(listen_id)
+    for _, v in ipairs(agent) do
+        skynet.send(v, "lua", "update")
+    end
 end
 
-if mode == "agent" then
-    skynet.start(function ()
-        skynet.dispatch("lua", function (_,_, id, protocol, addr)
-            local ok, err = websocket.accept(id, handle, protocol, addr)
-            if not ok then
-                print(err)
-            end
-        end)
-    end)
-else
-    skynet.start(function ()
-        for i= 1, agent_num do
-            agent[i] = skynet.newservice(SERVICE_NAME, port, wsapp_name, "agent")
+skynet.start(function ()
+    for i= 1, agent_num do
+        agent[i] = skynet.newservice("srv_websocket_agent", wsapp_name)
+    end
+
+    skynet.dispatch("lua", function(session, _, command, ...)
+        local f = CMD[command]
+        if session == 0 then
+            return f(...)
         end
-
-        skynet.dispatch("lua", function(session, _, command, ...)
-            local f = CMD[command]
-            if session == 0 then
-                return f(...)
-            end
-            skynet.ret(skynet.pack(f(...)))
-        end)
-
-        local balance = 1
-        local protocol = "ws"
-        local id = socket.listen("0.0.0.0", 9948)
-        listen_id = id
-        skynet.error(string.format("Listen websocket port 9948 protocol:%s", protocol))
-        socket.start(id, function(id, addr)
-            skynet.error(string.format("accept client socket_id: %s addr:%s", id, addr))
-            skynet.send(agent[balance], "lua", id, protocol, addr)
-            balance = balance + 1
-            if balance > #agent then
-                balance = 1
-            end
-        end)
+        skynet.ret(skynet.pack(f(...)))
     end)
-end
+
+    local balance = 1
+    local id = socket.listen("0.0.0.0", port)
+    listen_id = id
+    skynet.error("Listen websocket port ", port)
+    socket.start(id, function(fd, addr)
+        skynet.error(string.format("accept client fd: %s addr:%s", fd, addr))
+        skynet.send(agent[balance], "lua", "socket", fd, protocol, addr)
+        balance = balance + 1
+        if balance > #agent then
+            balance = 1
+        end
+    end)
+end)
---
+++ service/srv_websocket_agent.lua
@@ -0,0 +1,55 @@
+local skynet = require "skynet"
+local websocket = require "http.websocket"
+local logger = log4.get_logger(SERVICE_NAME)
+local SOCKET_NUMBER = 0
+
+local CMD = {}
+
+function add_web_agent_cmd(cmd, process)      -- 这样合适吗？
+    CMD[cmd] = process
+end
+
+local wsapp_name = ...
+local handle = require(wsapp_name)
+
+function CMD.update()
+    skynet.fork(function ()
+        while true do
+            skynet.sleep(60 * 100)           -- 60s
+            if SOCKET_NUMBER == 0 then
+                break
+            end
+        end
+        logger.info("after update service exit %08x", skynet.self())
+        skynet.exit()                        -- 没有连接存在了
+    end)
+end
+
+function CMD.info()
+    logger.info("socket connect number %s", SOCKET_NUMBER)
+end
+
+function CMD.socket(fd, protocol, addr)
+    SOCKET_NUMBER = SOCKET_NUMBER + 1
+    local ok, err = websocket.accept(fd, handle, protocol, addr)
+    if not ok then
+        print(err)
+    end
+    SOCKET_NUMBER = SOCKET_NUMBER - 1
+end
+
+skynet.start(function ()
+    skynet.dispatch("lua", function (session, _, command, ...)
+        local f = CMD[command]
+        if not f then
+            if session ~= 0 then
+                skynet.ret(skynet.pack(nil))
+            end
+            return
+        end
+        if session == 0 then
+            return f(...)
+        end
+        skynet.ret(skynet.pack(f(...)))
+    end)
+end)
